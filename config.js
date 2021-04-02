cdce18205301d26011e973c2cec0cc2ffbd64e2f
Purge-1 <80547719+Purge-1@users.noreply.github.com>
2021-04-02 21:14:04 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    token: 'ODE3NDQ0MjU1NDI5Njg5MzY2.YEJmSQ.feZjtHG-tJjdE6X6hpAQhEWsF60',
+    prefix: '+'
+}
+
+module.exports = config;
---
+++ events/ready.js
@@ -0,0 +1,6 @@
+module.exports = (client) => {
+	console.log('Loading....')
+    console.log('[LOGS]: Logged on as ' + client.user.tag)
+    console.log('[CREDITS]: Music Bot made by Purge')
+}
+// Dont remove credits or you will have some copyright issues.
